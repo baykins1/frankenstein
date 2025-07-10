7869307b25dd4fd845737b138c97e4e361848d0d
baykins1 <jacobtfulcher@gmail.com>
2025-07-10 16:07:53 -0500

    Renamed files, deleted broken ones

--- v1_esp_idf/main/esp_ot_cli.c
+++ v1_esp_idf/main/esp_ot_cli.c
@@ -4,41 +4,36 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-
 #include "sdkconfig.h"
+#include "hal/uart_types.h"
+#include "nvs_flash.h"
+#include "led_strip.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #include "esp_err.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_netif.h"
 #include "esp_netif_types.h"
+#include "esp_vfs_eventfd.h"
+#include "esp_timer.h"
+
 #include "esp_openthread.h"
 #include "esp_openthread_cli.h"
 #include "esp_openthread_lock.h"
 #include "esp_openthread_netif_glue.h"
 #include "esp_openthread_types.h"
 #include "esp_ot_config.h"
-#include "esp_vfs_eventfd.h"
-#include "driver/uart.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "hal/uart_types.h"
-#include "nvs_flash.h"
+
 #include "openthread/cli.h"
 #include "openthread/instance.h"
 #include "openthread/logging.h"
 #include "openthread/tasklet.h"
-#include "driver/gpio.h"
-#include "esp_timer.h"
 #include "openthread/udp.h"
-#include "led_strip.h"
-
-#if CONFIG_OPENTHREAD_STATE_INDICATOR_ENABLE
-#include "ot_led_strip.h"
-#endif
-
-#if CONFIG_OPENTHREAD_CLI_ESP_EXTENSION
-#include "esp_ot_cli_extension.h"
-#endif // CONFIG_OPENTHREAD_CLI_ESP_EXTENSION
 
 #define TAG "ot_esp_cli"
 
